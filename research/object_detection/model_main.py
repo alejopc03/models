b4a004b783aba21d3b45e160d7ee19456ed312e6
pereiraa <alejandro.pereira.calvo@intel.com>
2019-06-02 21:53:51 -0600

    initial development setup

--- research/object_detection/model_main.py
+++ research/object_detection/model_main.py
@@ -54,12 +54,12 @@ flags.DEFINE_boolean(
     'one round of eval vs running continuously (default).'
 )
 FLAGS = flags.FLAGS
-
+tf.logging.set_verbosity(tf.logging.INFO)
 
 def main(unused_argv):
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir)
+  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir,log_step_count_steps=1)
 
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,
